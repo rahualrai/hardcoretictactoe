1f60f2fde94ec6783a7c2c819a6da60014a51ae4
luminaa <rahual.rai@bison.howard.edu>
2023-02-15 14:06:13 -0500

    arduino

---
+++ arduino.ino
@@ -0,0 +1,26 @@
+#include "MeMegaPiPro.h" 
+
+int data;
+
+void setup() 
+{ 
+  Serial.begin(9600); 
+  pinMode(LED_BUILTIN, OUTPUT); 
+  digitalWrite (LED_BUILTIN, LOW);
+  Serial.println("Connected");
+}
+
+void loop() 
+{
+while (Serial.available())
+  {
+    data = Serial.read();
+  }
+
+  if (data == '1')
+  digitalWrite (LED_BUILTIN, HIGH);
+
+  else if (data == '0')
+  digitalWrite (LED_BUILTIN, LOW);
+
+}
